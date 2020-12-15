706475865ddacb6c9fe0de6105a5f6296557d38d
l337pino <1337pino@gmail.com>
2020-12-14 22:03:54 -0800

    Part 2 successfully finished

--- src/main/java/AdventOfCode2020/Day2.java
+++ src/main/java/AdventOfCode2020/Day2.java
@@ -1,7 +1,6 @@
 /* Day 2: Password Philosophy (https://adventofcode.com/2020/day/2)
 
 --- Part One ---
-
 Your flight departs in a few days from the coastal airport; the easiest way down to the coast from 
 here is via toboggan.
 
@@ -27,7 +26,27 @@ In the above example, 2 passwords are valid. The middle password, cdefg, is not;
 instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or
 nine c, both within the limits of their respective policies.
 
-How many passwords are valid according to their policies? */
+How many passwords are valid according to their policies?
+
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official 
+Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from 
+his old job at the sled rental place down the street! The Official Toboggan Corporate Policy 
+actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 
+means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of 
+"index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of 
+the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies? */
 
 import java.io.*;
 import java.util.*;
@@ -39,7 +58,7 @@ public class Day2 {
         + "\\src\\main\\java\\AdventOfCode2020\\input\\Day2.txt");
         List<String> passwordsAndPolicies = new ArrayList<String>();
 
-        int numberOfValidPasswords = 0;
+        int numberOfValidPasswordsPart1 = 0, numberOfValidPasswordsPart2 = 0;
         //////////////////////
 
         scanReport(passwordList, passwordsAndPolicies);
@@ -48,8 +67,8 @@ public class Day2 {
             String[] segments = line.split("[\\s-:]+"); // tokens are any blank spaces, - , or :
 
             // Password Policy details
-            int minimum = Integer.parseInt(segments[0]);
-            int maximum = Integer.parseInt(segments[1]);
+            int leftNum = Integer.parseInt(segments[0]);
+            int rightNum = Integer.parseInt(segments[1]);
             char letter = segments[2].charAt(0);
             String password = segments[3];
 
@@ -60,12 +79,19 @@ public class Day2 {
                 }
             }
 
-            if ((letterCount >= minimum) && (letterCount <= maximum)) {
-                numberOfValidPasswords++;
+            if ((letterCount >= leftNum) && (letterCount <= rightNum)) {
+                numberOfValidPasswordsPart1++;
+            }
+
+            // Exlcusive Or (XOR) for character at left or right number location in password
+            if ((password.charAt(leftNum - 1) == letter) 
+                ^ (password.charAt(rightNum - 1) == letter)) {
+                numberOfValidPasswordsPart2++;
             }
         }
 
-        System.out.println("Part 1 (number of valid passwords): " + numberOfValidPasswords);
+        System.out.println("Part 1 (number of valid passwords): " + numberOfValidPasswordsPart1);
+        System.out.println("Part 2 (number of valid passwords): " + numberOfValidPasswordsPart2);
     }
 
     /**
