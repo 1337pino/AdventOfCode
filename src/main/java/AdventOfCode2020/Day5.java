3d51b54ea4707179002f083492cff68ce3c3ec82
l337pino <1337pino@gmail.com>
2021-01-04 16:46:25 -0800

    Day 5 Part 1 initial working commit

---
+++ src/main/java/AdventOfCode2020/Day5.java
@@ -0,0 +1,146 @@
+/* Day 5: Binary Boarding (https://adventofcode.com/2020/day/5)
+
+--- Part One ---
+You board your plane only to discover a new problem: you dropped your boarding pass! You aren't 
+sure which seat is yours, and all of the flight attendants are busy with the flood of people that 
+suddenly made it through passport control.
+
+You write a quick program to use your phone's camera to scan all of the nearby boarding passes 
+(your puzzle input); perhaps you can find your seat through process of elimination.
+
+Instead of zones or groups, this airline uses binary space partitioning to seat people. A seat 
+might be specified like FBFBBFFRLR, where F means "front", B means "back", L means "left", and R 
+means "right".
+
+The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the 
+plane (numbered 0 through 127). Each letter tells you which half of a region the given seat is in. 
+Start with the whole list of rows; the first letter indicates whether the seat is in the front (0 
+through 63) or the back (64 through 127). The next letter indicates which half of that region the 
+seat is in, and so on until you're left with exactly one row.
+
+For example, consider just the first seven characters of FBFBBFFRLR:
+
+- Start by considering the whole range, rows 0 through 127.
+- F means to take the lower half, keeping rows 0 through 63.
+- B means to take the upper half, keeping rows 32 through 63.
+- F means to take the lower half, keeping rows 32 through 47.
+- B means to take the upper half, keeping rows 40 through 47.
+- B keeps rows 44 through 47.
+- F keeps rows 44 through 45.
+- The final F keeps the lower of the two, row 44.
+
+The last three characters will be either L or R; these specify exactly one of the 8 columns of 
+seats on the plane (numbered 0 through 7). The same process as above proceeds again, this time with 
+only three steps. L means to keep the lower half, while R means to keep the upper half.
+
+For example, consider just the last 3 characters of FBFBBFFRLR:
+
+- Start by considering the whole range, columns 0 through 7.
+- R means to take the upper half, keeping columns 4 through 7.
+- L means to take the lower half, keeping columns 4 through 5.
+- The final R keeps the upper of the two, column 5.
+
+So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
+
+Every seat also has a unique seat ID: multiply the row by 8, then add the column. In this example, 
+the seat has ID 44 * 8 + 5 = 357.
+
+Here are some other boarding passes:
+
+- BFFFBBFRRR: row 70, column 7, seat ID 567.
+- FFFBBBFRRR: row 14, column 7, seat ID 119.
+- BBFFBBFRLL: row 102, column 4, seat ID 820.
+
+As a sanity check, look through your list of boarding passes. What is the highest seat ID on a 
+boarding pass?
+*/
+
+import java.io.*;
+import java.util.*;
+
+
+public class Day5 {
+    public static void main(String[] args) {
+        // Declarations //////
+        File puzzleInputFile = new File(System.getProperty("user.dir") 
+            + "\\src\\main\\java\\AdventOfCode2020\\input\\Day5.txt");
+
+        ArrayList<String> boardingPassList = new ArrayList<String>();
+
+        int highestSeatIDFound = 0;
+        //////////////////////
+
+        // Load puzzle input data
+        scanListFile(puzzleInputFile, boardingPassList);
+
+        // Convert each seat info into seat ID
+        for (String boardingPass : boardingPassList) {
+            int seatID = translateBoardingPass(boardingPass);
+
+            // Update largest seat ID found if needed
+            if (seatID > highestSeatIDFound) highestSeatIDFound = seatID;
+        }
+
+        System.out.println("---------- DAY 5 2020 ----------");
+
+        System.out.println("Part 1 (highest seat ID found): " + highestSeatIDFound);
+    }
+
+    /**
+     * Translates a boarding pass into it's seat ID.
+     * 
+     * The first 7 characters will either be F or B (where F means "front" and B means "back"); 
+     * these specify exactly one of the 128 rows on the plane (numbered 0 through 127). The last 
+     * three characters will be either L or R (where L means "left", and R means "right"); these 
+     * specify exactly one of the 8 columns of seats on the plane (numbered 0 through 7).
+     * @param boardingPass String representation of the boarding pass.
+     * @return unique seat ID integer value
+     */
+    public static int translateBoardingPass(String boardingPass) {
+        String rowString = boardingPass.substring(0, 7), columnString = boardingPass.substring(7);
+
+        return (Integer.parseInt(convertBoardingStringToBinary(rowString), 2) * 8) 
+                + Integer.parseInt(convertBoardingStringToBinary(columnString), 2);
+    }
+
+    /**
+     * Converts a string representing a boarding pass row or column into a binary value.
+     * @param rowColumn string representing the row (using F|B) or column (using L|R)
+     * @return string of binary characters
+     */
+    private static String convertBoardingStringToBinary(String rowColumn) {
+        char[] rowColumnCharArray = rowColumn.toCharArray();
+
+        for (int i = 0; i < rowColumnCharArray.length; i++) {
+            if ((rowColumnCharArray[i] == 'F') || (rowColumnCharArray[i] == 'L')) {
+                rowColumnCharArray[i] = '0';
+            }
+            if ((rowColumnCharArray[i] == 'B') || (rowColumnCharArray[i] == 'R')) {
+                rowColumnCharArray[i] = '1';
+            }
+        }
+
+        return new String(rowColumnCharArray);
+    }
+
+    /**
+     * Scans the list of boarding passes and loads all of the strings into a list
+     * @param inputFile text file for the list of boarding passes
+     * @param passwordAndPolicy List structure for boarding passes
+     */
+    public static void scanListFile(File inputFile, List<String> boardingPassList) {
+        Scanner lineTokenizer = null;
+        try {
+            lineTokenizer = new Scanner(inputFile);
+
+            while (lineTokenizer.hasNextLine()) {
+                String boardingPass = lineTokenizer.nextLine();
+                boardingPassList.add(boardingPass);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            lineTokenizer.close();
+        }
+    }
+}
